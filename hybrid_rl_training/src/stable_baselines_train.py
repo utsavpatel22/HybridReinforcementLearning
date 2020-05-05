cba0ab355039f539a21f60ec84342fa3cd520e76
utsavpatel22 <erutsavpatel22@gmail.com>
2020-05-05 12:30:47 -0400

    Added training script to train multiple agents

--- hybrid_rl_training/src/stable_baselines_train.py
+++ hybrid_rl_training/src/stable_baselines_train.py
@@ -3,17 +3,18 @@
 import sys
 sys.path.remove('/opt/ros/kinetic/lib/python2.7/dist-packages')
 import gym
-from stable_baselines.common.env_checker import check_env
 from stable_baselines.common.policies import MlpPolicy
+from stable_baselines.common.vec_env import SubprocVecEnv
 from stable_baselines import PPO2
 sys.path.append('/opt/ros/kinetic/lib/python2.7/dist-packages')
-from openai_ros.task_envs.turtlebot2 import turtlebot2_maze
+from openai_ros.task_envs.turtlebot2.turtlebot2_maze import TurtleBot2MazeEnv
 import rospy
 import os
 
-rospy.init_node('stable_training', anonymous=True, log_level=rospy.WARN)
-
-env = gym.make('turtlebot-v0')
-model = PPO2(MlpPolicy, env, tensorboard_log="../PPO2_turtlebot_tensorboard/", verbose=1)
-model.learn(total_timesteps=25000)
-model.save("ppo2_cartpole")
+if __name__ == '__main__':
+	rospy.init_node('stable_training', anonymous=True, log_level=rospy.WARN)
+	env_temp = TurtleBot2MazeEnv
+	env = SubprocVecEnv([lambda k=k:env_temp(k) for k in range(4)])
+	model = PPO2(MlpPolicy, env, tensorboard_log="../PPO2_turtlebot_tensorboard/", verbose=1)
+	model.learn(total_timesteps=25000)
+	model.save("ppo2_cartpole")
