83758e0e400e0119628799db8999dc900eee4fee
utsavpatel22 <erutsavpatel22@gmail.com>
2020-05-12 00:45:27 -0400

    Modified DWA scripts such that it outputs v,w and cost

---
+++ openai_ros/openai_ros/src/openai_ros/task_envs/turtlebot2/dwa.py
@@ -0,0 +1,164 @@
+#!/usr/bin/env python
+
+import math
+import numpy as np
+import time
+
+
+# Model to determine the expected position of the robot after moving along trajectory
+def motion(x, u, dt):
+    # motion model
+    # x = [x(m), y(m), theta(rad), v(m/s), omega(rad/s)]
+    x[2] += u[1] * dt
+    x[0] += u[0] * math.cos(x[2]) * dt
+    x[1] += u[0] * math.sin(x[2]) * dt
+    
+    x[3] = u[0]
+    x[4] = u[1]
+
+    return x
+
+# Determine the dynamic window from robot configurations
+def calc_dynamic_window(x, config):
+
+    # Dynamic window from robot specification
+    Vs = [config.min_speed, config.max_speed,
+          -config.max_yawrate, config.max_yawrate]
+
+    # Dynamic window from motion model
+    Vd = [x[3] - config.max_accel * config.dt,
+          x[3] + config.max_accel * config.dt,
+          x[4] - config.max_dyawrate * config.dt,
+          x[4] + config.max_dyawrate * config.dt]
+
+    #  [vmin, vmax, yawrate min, yawrate max]
+    dw = [max(Vs[0], Vd[0]), min(Vs[1], Vd[1]),
+          max(Vs[2], Vd[2]), min(Vs[3], Vd[3])]
+
+    return dw
+
+# Calculate a trajectory sampled across a prediction time
+def calc_trajectory(xinit, v, y, config):
+
+    x = np.array(xinit)
+    traj = np.array(x)  # many motion models stored per trajectory
+    time = 0
+    while time <= config.predict_time:
+        # store each motion model along a trajectory
+        x = motion(x, [v, y], config.dt)
+        traj = np.vstack((traj, x))
+        time += config.dt # next sample
+
+    return traj
+
+# Calculate trajectory, costings, and return velocities to apply to robot
+def calc_final_input(x, u, dw, config, ob):
+
+    xinit = x[:]
+    min_cost = 10000.0
+    min_u = u
+    min_u[0] = 0.0
+    v_list = []
+    w_list = []
+    cost_list = []
+
+    # evaluate all trajectory with sampled input in dynamic window
+    for v in np.arange(dw[0], dw[1], config.v_reso):
+        for w in np.arange(dw[2], dw[3], config.yawrate_reso):
+            traj = calc_trajectory(xinit, v, w, config)
+
+            # calc costs with weighted gains
+            to_goal_cost = calc_to_goal_cost(traj, config) * config.to_goal_cost_gain
+            speed_cost = config.speed_cost_gain * \
+                (config.max_speed - traj[-1, 3])
+
+            ob_cost = calc_obstacle_cost(traj, ob, config) * config.obs_cost_gain
+
+            final_cost = to_goal_cost + speed_cost + ob_cost
+            v_list.append(v)
+            w_list.append(w)
+            cost_list.append(final_cost)
+
+    return v_list, w_list, cost_list
+
+# Calculate obstacle cost inf: collision, 0:free
+def calc_obstacle_cost(traj, ob, config):
+    skip_n = 2
+    minr = float("inf")
+
+    # Loop through every obstacle in set and calc Pythagorean distance
+    # Use robot radius to determine if collision
+    for ii in range(0, len(traj[:, 1]), skip_n):
+        for i in ob.copy():
+            ox = i[0]
+            oy = i[1]
+            dx = traj[ii, 0] - ox
+            dy = traj[ii, 1] - oy
+
+            r = math.sqrt(dx**2 + dy**2)
+
+            if r <= config.robot_radius:
+                return float("Inf")  # collision
+
+            if minr >= r:
+                minr = r
+
+    return 1.0 / minr
+
+# Calculate goal cost via Pythagorean distance to robot
+def calc_to_goal_cost(traj, config):
+    # If-Statements to determine negative vs positive goal/trajectory position
+    # traj[-1,0] is the last predicted X coord position on the trajectory
+    if (config.goalX >= 0 and traj[-1,0] < 0):
+        dx = config.goalX - traj[-1,0]
+    elif (config.goalX < 0 and traj[-1,0] >= 0):
+        dx = traj[-1,0] - config.goalX
+    else:
+        dx = abs(config.goalX - traj[-1,0])
+    # traj[-1,1] is the last predicted Y coord position on the trajectory
+    if (config.goalY >= 0 and traj[-1,1] < 0):
+        dy = config.goalY - traj[-1,1]
+    elif (config.goalY < 0 and traj[-1,1] >= 0):
+        dy = traj[-1,1] - config.goalY
+    else:
+        dy = abs(config.goalY - traj[-1,1])
+
+    cost = math.sqrt(dx**2 + dy**2)
+    return cost
+
+# Begin DWA calculations
+def dwa_control(x, u, config, ob):
+    # Dynamic Window control
+
+    dw = calc_dynamic_window(x, config)
+
+    u = calc_final_input(x, u, dw, config, ob)
+
+    return u
+
+# Determine whether the robot has reached its goal
+def atGoal(config, x):
+    # check at goal
+    if math.sqrt((x[0] - config.goalX)**2 + (x[1] - config.goalY)**2) \
+        <= config.robot_radius:
+        return True
+    return False
+
+
+def DWA(config, obstacles):
+   
+    # initial state [x(m), y(m), theta(rad), v(m/s), omega(rad/s)]
+    x = np.array([config.x, config.y, config.th, config.v, config.omega])
+    # initial linear and angular velocities
+    u = np.array([0.0, 0.0])
+
+    start_time = time.time()
+    v_list, w_list, cost_list = dwa_control(x, u, config, obstacles.obst)
+    
+    
+    return v_list, w_list, cost_list
+       
+
+
+
+
