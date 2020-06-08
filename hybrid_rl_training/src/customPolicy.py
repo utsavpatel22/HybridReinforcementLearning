6907e469d2022439948b3689614ad7e3778a28f7
utsavpatel22 <erutsavpatel22@gmail.com>
2020-06-08 15:11:31 -0400

    Added a somewhat deep custom policy and modifed the name of the shallow policy

--- hybrid_rl_training/src/customPolicy.py
+++ hybrid_rl_training/src/customPolicy.py
@@ -4,7 +4,7 @@ from stable_baselines.common.tf_layers import conv, linear, conv_to_fc, lstm
 import numpy as np
 
 
-def modified_small_nature_cnn(scaled_images, **kwargs):
+def modified_shallow_nature_cnn(scaled_images, **kwargs):
     """
     CNN from Nature paper.
     :param scaled_images: (TensorFlow Tensor) Image input placeholder
@@ -17,9 +17,29 @@ def modified_small_nature_cnn(scaled_images, **kwargs):
     layer_2 = conv_to_fc(layer_2)
     return activ(linear(layer_2, 'fc1', n_hidden=128, init_scale=np.sqrt(2)))
 
+def modified_deep_nature_cnn(scaled_images, **kwargs):
+    """
+    CNN from Nature paper.
+    :param scaled_images: (TensorFlow Tensor) Image input placeholder
+    :param kwargs: (dict) Extra keywords parameters for the convolutional layers of the CNN
+    :return: (TensorFlow Tensor) The CNN output layer
+    """
+    activ = tf.nn.relu
+    layer_1 = activ(conv(scaled_images, 'c1', n_filters=8, filter_size=6, stride=1, init_scale=np.sqrt(2), **kwargs))
+    layer_2 = activ(conv(layer_1, 'c2', n_filters=16, filter_size=3, stride=1, init_scale=np.sqrt(2), **kwargs))
+    layer_2 = conv_to_fc(layer_2)
+    layer_3 = activ(linear(layer_2, 'fc1', n_hidden=128, init_scale=np.sqrt(2)))
+    return activ(linear(layer_3, 'fc2', n_hidden=128, init_scale=np.sqrt(2)))
 
 
-class CustomCNNPolicy(common.FeedForwardPolicy):
+
+class CustomShallowCNNPolicy(common.FeedForwardPolicy):
     
     def __init__(self, *args, **kwargs):
-        super(CustomCNNPolicy, self).__init__(*args, **kwargs, cnn_extractor=modified_nature_cnn, feature_extraction="cnn")
+        super(CustomShallowCNNPolicy, self).__init__(*args, **kwargs, cnn_extractor=modified_shallow_nature_cnn, feature_extraction="cnn")
+
+
+class CustomDeepCNNPolicy(common.FeedForwardPolicy):
+    
+    def __init__(self, *args, **kwargs):
+        super(CustomDeepCNNPolicy, self).__init__(*args, **kwargs, cnn_extractor=modified_deep_nature_cnn, feature_extraction="cnn")
