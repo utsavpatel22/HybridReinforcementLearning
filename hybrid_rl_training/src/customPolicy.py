9c17f8e2c0d18d3811a09c6cc7306242609cd76e
nithishkumarS <nithishtimes@gmail.com>
2020-06-08 20:30:06 -0400

    Updated input arguements to custom policy super

--- hybrid_rl_training/src/customPolicy.py
+++ hybrid_rl_training/src/customPolicy.py
@@ -29,9 +29,17 @@ class CustomCNNPolicy(common.FeedForwardPolicy):
 
 
 class CustomLSTMPolicy(LstmPolicy):
-    def __init__(self, sess, ob_space, ac_space, n_env, n_steps, n_batch, n_lstm=64, reuse=False, **_kwargs):
-        super().__init__(sess, ob_space, ac_space, n_env, n_steps, n_batch, n_lstm, reuse,
+    def __init__(self, *args, n_lstm=64, reuse=False, **_kwargs):
+        super().__init__(*args, n_lstm, reuse,
                          net_arch=[8, 'lstm', dict(vf=[5, 10], pi=[10])],
-                         layer_norm=True, feature_extraction="mlp", **_kwargs)
+                         layer_norm=True, feature_extraction="cnn", cnn_extractor=modified_nature_cnn,**_kwargs)
 
-        cnn_extractor=modified_nature_cnn
+
+# super(FeedForwardPolicy, self).__init__(sess, ob_space, ac_space, n_env, n_steps, n_batch, reuse=reuse,
+#                                                 scale=(feature_extraction == "cnn"))
+#
+#
+# def __init__(self, sess, ob_space, ac_space, n_env, n_steps, n_batch, n_lstm=64, reuse=False, **_kwargs):
+#     super().__init__(sess, ob_space, ac_space, n_env, n_steps, n_batch, n_lstm, reuse,
+#                      net_arch=[8, 'lstm', dict(vf=[5, 10], pi=[10])],
+#                      layer_norm=True, feature_extraction="mlp", **_kwargs)
