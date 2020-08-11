e64ebea4fe718e8d76a10f32c736745c69622d45
utsavpatel22 <erutsavpatel22@gmail.com>
2020-08-11 17:15:04 -0400

    [turtlebot2_maze]: Reduced the collision distance to 0.5m and increased skipped frames to 6

--- openai_ros/openai_ros/src/openai_ros/task_envs/turtlebot2/turtlebot2_maze.py
+++ openai_ros/openai_ros/src/openai_ros/task_envs/turtlebot2/turtlebot2_maze.py
@@ -65,11 +65,11 @@ class TurtleBot2MazeEnv(turtlebot2_env.TurtleBot2Env):
         
         self.n_laser_discretization = rospy.get_param('/turtlebot2/n_laser_discretization',128)
         self.n_observations = rospy.get_param('/turtlebot2/n_observations',144)
-        self.min_range = rospy.get_param('/turtlebot2/min_range',0.8)
+        self.min_range = rospy.get_param('/turtlebot2/min_range',0.5)
         self.max_cost = rospy.get_param('/turtlebot2/max_cost',1)
         self.min_cost = rospy.get_param('/turtlebot2/min_cost',0)
         self.n_stacked_frames = rospy.get_param('/turtlebot2/n_stacked_frames',10)
-        self.n_skipped_frames = rospy.get_param('/turtlebot2/n_skipped_frames',4)
+        self.n_skipped_frames = rospy.get_param('/turtlebot2/n_skipped_frames',6)
 
         self.max_linear_speed = rospy.get_param('/turtlebot2/max_linear_speed',0.65)
         self.max_angular_speed = rospy.get_param('/turtlebot2/max_angular_speed',1)
