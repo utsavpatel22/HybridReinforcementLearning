fa7f0b2d1968f7bce93f4c2be9ab86acd7bd3950
HybridRL <nithishtimes@gmail.com>
2020-10-06 16:52:20 -0400

    Updated parameters

--- hybrid_rl_training/src/test_trained_model.py
+++ hybrid_rl_training/src/test_trained_model.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python
 
 import sys
-sys.path.remove('/opt/ros/kinetic/lib/python2.7/dist-packages')
+sys.path.remove('/opt/ros/melodic/lib/python2.7/dist-packages')
 import gym
 from stable_baselines.common.policies import FeedForwardPolicy, register_policy
 from stable_baselines.common.vec_env import SubprocVecEnv
 from stable_baselines import PPO2
-sys.path.append('/opt/ros/kinetic/lib/python2.7/dist-packages')
+sys.path.append('/opt/ros/melodic/lib/python2.7/dist-packages')
 from openai_ros.task_envs.turtlebot2.turtlebot2_maze import TurtleBot2MazeEnv
 import rospy
 import os
@@ -31,14 +31,15 @@ if __name__ == '__main__':
     world_file = sys.argv[1]
     number_of_robots = sys.argv[2]
     robot_number = sys.argv[3] # Provide robot number to subscribe to the correct topic  
+    profiling = True
     max_steps = 900
-    max_test_episodes = 2
+    max_test_episodes = 50
     min_range = 0.5 # Refer Task environment to get the value of min range
     rospy.init_node('stable_training', anonymous=True, log_level=rospy.WARN)
     TotalDistance = rospy.Subscriber("/total_distance",Float32, getDistance)
     env_temp = TurtleBot2MazeEnv
     env = SubprocVecEnv([lambda k=k:env_temp(world_file, k) for k in range(int(number_of_robots))])
-    model = PPO2.load("ppo2_turtlebot")
+    model = PPO2.load("TrainedModels/ppo2_turtlebot#5")
 
     counter = 0
     collisions = 0
@@ -58,14 +59,15 @@ if __name__ == '__main__':
             if (done):
                 print("Done")
                 counter += 1
-                total_time_episode = rospy.get_time() - start_time
-                total_distance_episode = td_before_reset - start_td
-                print("The total time is {}".format(total_time_episode))
-                print("The distance travelled {}".format(total_distance_episode))
-                episode_time_list.append(total_time_episode)
-                episode_dist_list.append(total_distance_episode)
-                start_time = rospy.get_time()
-                start_td = td
+                if profiling:
+                    total_time_episode = rospy.get_time() - start_time
+                    total_distance_episode = td_before_reset - start_td
+                    print("The total time is {}".format(total_time_episode))
+                    print("The distance travelled {}".format(total_distance_episode))
+                    episode_time_list.append(total_time_episode)
+                    episode_dist_list.append(total_distance_episode)
+                    start_time = rospy.get_time()
+                    start_td = td
                 break
 
     print("Total number of collisions {}".format(collisions))
