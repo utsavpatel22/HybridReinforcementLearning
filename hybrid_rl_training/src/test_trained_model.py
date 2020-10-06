c0a0c428db3ee3b6a5b7699ba3dbcd317a17972c
utsavpatel22 <erutsavpatel22@gmail.com>
2020-10-06 16:42:02 -0400

    [test_trained_model]: The script now saves the distance and time for each episode in a csv file

--- hybrid_rl_training/src/test_trained_model.py
+++ hybrid_rl_training/src/test_trained_model.py
@@ -14,6 +14,7 @@ from customPolicy import *
 from sensor_msgs.msg import LaserScan
 import time
 from std_msgs.msg import Float32
+import csv
 # Custom MLP policy of three layers of size 128 each
 class CustomPolicy(FeedForwardPolicy):
     def __init__(self, *args, **kwargs):
@@ -32,7 +33,7 @@ if __name__ == '__main__':
     number_of_robots = sys.argv[2]
     robot_number = sys.argv[3] # Provide robot number to subscribe to the correct topic  
     max_steps = 900
-    max_test_episodes = 2
+    max_test_episodes = 1
     min_range = 0.5 # Refer Task environment to get the value of min range
     rospy.init_node('stable_training', anonymous=True, log_level=rospy.WARN)
     TotalDistance = rospy.Subscriber("/total_distance",Float32, getDistance)
@@ -43,8 +44,7 @@ if __name__ == '__main__':
     counter = 0
     collisions = 0
     start_time = rospy.get_time()
-    episode_time_list = []
-    episode_dist_list = []
+    episode_time_dist_list = []
     start_td = td
     while(counter < max_test_episodes):
         obs = env.reset()
@@ -62,12 +62,16 @@ if __name__ == '__main__':
                 total_distance_episode = td_before_reset - start_td
                 print("The total time is {}".format(total_time_episode))
                 print("The distance travelled {}".format(total_distance_episode))
-                episode_time_list.append(total_time_episode)
-                episode_dist_list.append(total_distance_episode)
+                episode_time_dist_list.append([total_time_episode, total_distance_episode])
+
                 start_time = rospy.get_time()
                 start_td = td
                 break
 
     print("Total number of collisions {}".format(collisions))
-    print(episode_time_list)
+    file = open('trained_model_data.csv', 'w')
+    with file:     
+      write = csv.writer(file) 
+      write.writerows(episode_time_dist_list)
+
         
