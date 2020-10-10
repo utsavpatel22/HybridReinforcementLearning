f919cd0bfbd7197d00f4a80881f16255fb1e43de
utsavpatel22 <erutsavpatel22@gmail.com>
2020-10-10 12:07:22 -0400

    [test_trained_model] [turtlebot2_maze]: Added goal reaching status in the test scripts for the trained model

--- hybrid_rl_training/src/test_trained_model.py
+++ hybrid_rl_training/src/test_trained_model.py
@@ -13,7 +13,7 @@ import os
 from customPolicy import *
 from sensor_msgs.msg import LaserScan
 import time
-from std_msgs.msg import Float32
+from std_msgs.msg import Float32, Bool
 import csv
 # Custom MLP policy of three layers of size 128 each
 class CustomPolicy(FeedForwardPolicy):
@@ -28,15 +28,21 @@ def getDistance(msg):
     global td
     td = msg.data
 
+gr = False
+def getGoalReachingStatus(msg):
+    global gr
+    gr = msg.data
+
 if __name__ == '__main__':
     world_file = sys.argv[1]
     number_of_robots = sys.argv[2]
     robot_number = sys.argv[3] # Provide robot number to subscribe to the correct topic  
     max_steps = 900
-    max_test_episodes = 1
+    max_test_episodes = 2
     min_range = 0.5 # Refer Task environment to get the value of min range
     rospy.init_node('stable_training', anonymous=True, log_level=rospy.WARN)
     TotalDistance = rospy.Subscriber("/total_distance",Float32, getDistance)
+    goal_reaching_status = rospy.Subscriber('/turtlebot'+str(robot_number)+'/goal_reaching_status', Bool, getGoalReachingStatus)
     env_temp = TurtleBot2MazeEnv
     env = SubprocVecEnv([lambda k=k:env_temp(world_file, k) for k in range(int(number_of_robots))])
     model = PPO2.load("ppo2_turtlebot")
@@ -46,6 +52,7 @@ if __name__ == '__main__':
     start_time = rospy.get_time()
     episode_time_dist_list = []
     start_td = td
+    goal_reached = False
     while(counter < max_test_episodes):
         obs = env.reset()
         # Evaluate the agent
@@ -55,6 +62,7 @@ if __name__ == '__main__':
             td_before_reset = td
             obs, reward, done, info = env.step(action)
             episode_reward += reward
+            goal_reached = gr
             if (done):
                 print("Done")
                 counter += 1
@@ -62,7 +70,7 @@ if __name__ == '__main__':
                 total_distance_episode = td_before_reset - start_td
                 print("The total time is {}".format(total_time_episode))
                 print("The distance travelled {}".format(total_distance_episode))
-                episode_time_dist_list.append([total_time_episode, total_distance_episode])
+                episode_time_dist_list.append([total_time_episode, total_distance_episode,goal_reached])
 
                 start_time = rospy.get_time()
                 start_td = td
