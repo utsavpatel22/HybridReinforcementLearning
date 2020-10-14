4f62e7beb5c9f0b16e59889eee9cf9d84249568a
NithishkumarS <nithishtimes@gmail.com>
2020-10-13 22:03:38 -0400

    Updated goalpub to take waypoints

--- frozone/src/relative_goal_pub.cpp
+++ frozone/src/relative_goal_pub.cpp
@@ -14,26 +14,37 @@ private:
   ros::NodeHandle nh;
   ros::Publisher pub;
   ros::Subscriber sub;
+  ros::Subscriber location;
 
 public:
   // Data members
   float goal_x;
   float goal_y;
+  float waypoint[4];
+  int count;
+
 
   // Member functions
   // Constructor
   Goal_Pub(){
     init_sub();
     init_pub();
+    float y[5] = {3,6,9,12.5};
+
     goal_x = 12.5;
     goal_y = 0.0;
+    waypoint[0] = 3.0;
+    waypoint[1] = 6.0;
+    waypoint[2]  =9.0;
+    waypoint[3] = 12.5;
+    count = 0;
     ROS_INFO("Inside Contructor");
   }
 
   void init_sub() {
     ROS_INFO("Initializing Subscribers");
     sub = nh.subscribe("/turtlebot0/ground_truth/state", 100, &Goal_Pub::stateCb, this);
-  }
+    }
 
   void init_pub() {
     ROS_INFO("Initializing Publishers");
@@ -48,6 +59,11 @@ public:
       // std::cout<< "Jackal Position = " << msg.pose[i].position.x << "   " << msg.pose[i].position.y << std::endl;
       rel_goal.linear.x = goal_x - msg.pose.pose.position.x;
       rel_goal.linear.y = goal_y - msg.pose.pose.position.y;
+
+      if((goal_x - msg.pose.pose.position.x)*(goal_x - msg.pose.pose.position.x) + (goal_y - (msg.pose.pose.position.y))*(goal_y - (msg.pose.pose.position.y)) < .25){
+        goal_x = waypoint[count];
+        count++;
+      }
 
       tf::Pose pose;
       tf::poseMsgToTF(msg.pose.pose, pose);
