bbf9e184c5ea910d7d106776390f1531a002951f
757670303037 <utsavvpatel22@gmail.com>
2022-02-01 14:34:24 -0500

    Added code to calculate running average

--- hybrid_rl_training/src/TRT_action_publisher.py
+++ hybrid_rl_training/src/TRT_action_publisher.py
@@ -7,6 +7,7 @@ from stable_baselines import PPO2
 from std_msgs.msg import Int64
 import tensorflow as tf
 from tensorflow.python.compiler.tensorrt import trt_convert as trt
+import time
 
 obs = np.array([])
 def getObservation(msg):
@@ -33,10 +34,17 @@ if __name__ == '__main__':
 		# converter.save(output_saved_model_dir='/home/thebeast/workspaces/TFSaveTest/TensorRT/') # Does not work
 		# Import the TensorRT graph into a new graph and run:
 		output_node = tf.import_graph_def(trt_graph, return_elements=['output/ArgMax:0'])
+		n = 0
+		total_time = 0
 		while not rospy.is_shutdown():
 			if obs.shape[0] != 0:
+				n = n+1
 				obs_sub = np.reshape(obs, (1,144,10,4))
+				start_t = time.time()
 				action = sess.run(output_node, {'import/input/Ob:0': obs_sub})
+				total_time = total_time + (time.time() - start_t)
+				avg_time = total_time / n
+				print("The average time for prediction ", avg_time)
 				action_publisher.publish(action[0][0])
 				print("Publishing the action")
 
--- hybrid_rl_training/src/action_publisher.py
+++ hybrid_rl_training/src/action_publisher.py
@@ -5,6 +5,7 @@ from rospy.numpy_msg import numpy_msg
 from rospy_tutorials.msg import Floats
 from stable_baselines import PPO2
 from std_msgs.msg import Int64
+import time
 
 obs = np.array([])
 def getObservation(msg):
@@ -16,9 +17,16 @@ if __name__ == '__main__':
 	model = PPO2.load("ppo2_turtlebot#5")
 	action_publisher = rospy.Publisher("/predicted_action", Int64,queue_size=10)
 	observation_subscriber = rospy.Subscriber("/obs_data", numpy_msg(Floats), getObservation)
+	n = 0
+	total_time = 0
 	while not rospy.is_shutdown():
 		if obs.shape[0] != 0:
+			n = n+1 
 			obs_sub = np.reshape(obs, (1,144,10,4))
+			start_t = time.time()
 			action, _ = model.predict(obs_sub)
+			total_time = total_time + (time.time() - start_t)
+			avg_time = total_time / n 
+			print("The average time for prediction ", avg_time) 
 			action_publisher.publish(action[0])
 			print("Publishing the action")
